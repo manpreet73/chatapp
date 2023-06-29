81338c8c1516719bccaa941d463a10f16bea8d4c
Manpreet <manpreet@atlassian.com>
2023-06-29 23:58:55 +0530

    created reusable editable input

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,22 @@
 
-import { Button, Drawer, Icon } from 'rsuite'
+import { Alert, Button, Drawer, Icon } from 'rsuite'
 import { useMediaQuery, useModalState } from '../../misc/customHooks'
 import Dashboard from '.';
+import { useCallback } from 'react';
+import {auth} from '../../misc/firebase'
 
 const DashboardToggle = () => {
 
   const {isOpen,open,close}= useModalState();
   const isMobile=useMediaQuery('(max-width:992px)')
+
+  const onSignOut=useCallback(()=>{
+
+    auth.signOut();
+    Alert.info('Signed out',4000);
+    close();
+
+  },[close])
 
   return (
     <>
@@ -14,7 +24,7 @@ const DashboardToggle = () => {
         <Icon icon='dashboard'/> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement='left'>
-      <Dashboard/>
+      <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   )
