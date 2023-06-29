f68a46c2cfd8a6f560041d8a84bb5c330e77a255
Manpreet <manpreet@atlassian.com>
2023-06-29 23:00:11 +0530

    initialized sidebar

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,23 @@
+
+import { Button, Drawer, Icon } from 'rsuite'
+import { useMediaQuery, useModalState } from '../../misc/customHooks'
+import Dashboard from '.';
+
+const DashboardToggle = () => {
+
+  const {isOpen,open,close}= useModalState();
+  const isMobile=useMediaQuery('(max-width:992px)')
+
+  return (
+    <>
+      <Button block color='blue' onClick={open}>
+        <Icon icon='dashboard'/> Dashboard
+      </Button>
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement='left'>
+      <Dashboard/>
+      </Drawer>
+    </>
+  )
+}
+
+export default DashboardToggle
