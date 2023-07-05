8f8f036934d33944160c04d59f3343ddaa298411
Manpreet <manpreet@atlassian.com>
2023-07-05 22:22:57 +0530

    created chat room list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,27 @@
+import { Divider } from 'rsuite'
 import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal'
 import DashboardToggle from './dashboard/DashboardToggle'
+import ChatRoomList from './rooms/ChatRoomList'
+import { useState ,useRef, useEffect} from 'react'
 
 const Sidebar = () => {
+  const topSidebarRef=useRef();
+  const [height,setHeight]=useState(0);
+
+  useEffect(()=>{
+    if(topSidebarRef.current){
+      setHeight(topSidebarRef.current.scrollHeight)
+    }
+  },[topSidebarRef])
+
   return (
     <div className='h-100 pt-2'>
-        <div>
+        <div ref={topSidebarRef}>
             <DashboardToggle/>
             <CreateRoomBtnModal/>
+            <Divider>Join conversation</Divider>
         </div>
-      
+      <ChatRoomList aboveElemHeight={height}/>
     </div>
   )
 }
---
+++ src/components/rooms/ChatRoomList.js
@@ -0,0 +1,23 @@
+import { Loader, Nav } from "rsuite"
+import RoomItem from './RoomItem'
+import { useRooms } from "../../context/rooms.context"
+import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+const ChatRoomList = ({ aboveElemHeight }) => {
+    const rooms = useRooms();
+    const location = useLocation();
+
+    return <Nav appearance="subtle" vertical reversed className="overflow-y-scroll custom-scroll" style={{ height: `calc(100% - ${aboveElemHeight}px)`, }} activeKey={location.pathname}>
+        {!rooms && (
+            <Loader center vertical content='Loading' speed="slow" size='md'/>
+        )}
+        {rooms && rooms.length > 0 && rooms.map(room=>(
+            <Nav.Item componentClass={Link} to={`/chat/${room.id}`} key={room.id} eventKey={`/chat/${room.id}`}>
+                <RoomItem room={room} />
+            </Nav.Item>
+        ))}
+        </Nav>
+    
+}
+
+export default ChatRoomList
