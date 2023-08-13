38e01f778aa77ff4649ec9089cbea14cf24ffccd
Manpreet Kaur <136912769+manpreet73@users.noreply.github.com>
2023-08-13 14:49:08 +0530

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -45,9 +45,9 @@ const SignIn = () => {
                             <Button block color="green" onClick={onGoogleSignIn}>
                                 <Icon icon="google"/> Continue with Google
                             </Button>
-                            // <Button block color="blue" onClick={onFacebookSignIn}>
-                            //     <Icon icon="facebook"/> Continue with Facebook
-                            // </Button>
+                           {/* <Button block color="blue" onClick={onFacebookSignIn}>
+                                <Icon icon="facebook"/> Continue with Facebook
+                            </Button> */}
                         </div>
                     </Panel>
                 </Col>
